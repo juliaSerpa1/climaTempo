edcb2aec7cd69f596cabdc00f9a9d78a8af9b796
juliaSerpa1 <julia_jmsn@hotmail.com>
2023-02-20 10:21:06 -0400

    adicionando botão e pegando dados do input

---
+++ README.md
@@ -0,0 +1,3 @@
+Projeto Clima Tempo
+
+Software que mostra o clima de acordo com a cidade selecionada.
---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+// Variaveis e seleção de elementos
+const apiKey = "ba88f11f29e67ba2ae29fcc7d087386f";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
+// Funções 
+const getWeatherData = async(city) => {
+
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL)
+    const data = await res.json()
+
+    console.log(data);
+}
+
+const showWeatherData = (city)=>{
+    getWeatherData(city)
+}
+
+// Eventos
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+    
+    showWeatherData(city)
+});
